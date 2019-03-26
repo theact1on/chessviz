680d819b0e80607a58fd28d8d530c6ff407a55e7
Maxim Luparev <luparev22@list.ru>
2019-03-26 18:13:46 +0700

    Add code for open or create chessviz.html file

--- src/board_print_html.cpp
+++ src/board_print_html.cpp
@@ -2,4 +2,8 @@
 
 void outputHTML(char output_board[][8], char str_info[])
 {
+    FILE* output_html_write;
+    output_html_write = fopen("chessviz.html", "a+");
+
+    fclose(output_html_write);
 }
