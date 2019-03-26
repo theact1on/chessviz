b2f339376365d647315fce1c59fedc88fd9d6c40
Maxim Luparev <luparev22@list.ru>
2019-03-26 19:20:33 +0700

    Added check for line matching (simple move or take)

--- src/board.cpp
+++ src/board.cpp
@@ -199,4 +199,42 @@ void moveFigures(step_white* white_step, char board[][8])
         printf("ERROR>>FIELD_NOT_FOUND\nExiting...\n" ANSI_COLOR_RESET);
         exit(1);
     }
+
+    int step_liter = (int)white_step->from[0] - 97;
+    int step_digit = 8 - ((int)white_step->from[1] - 48) % 9;
+
+    if (board[step_digit][step_liter] == white_step->figure) {
+        board[step_digit][step_liter] = ' ';
+        if (white_step->how == 'x') {
+            step_liter = (int)white_step->to[0] - 97;
+            step_digit = 8 - ((int)white_step->to[1] - 48) % 9;
+            if (board[step_digit][step_liter] == ' ') {
+                printf(ANSI_COLOR_RED
+                       "In " ANSI_COLOR_GREEN "%s" ANSI_COLOR_RED
+                       " line. ERROR. Type of stroke " ANSI_COLOR_GREEN
+                       "%c" ANSI_COLOR_RED ", but in field " ANSI_COLOR_GREEN
+                       "%s" ANSI_COLOR_RED " dont have figure.\n",
+                       white_step->num,
+                       white_step->how,
+                       white_step->to);
+                printf("ERROR>>TAKE_BUT_FIELD_DONT_HAVE_FIGURE\nExiting..."
+                       "\n" ANSI_COLOR_RESET);
+            }
+        }
+        step_liter = (int)white_step->to[0] - 97;
+        step_digit = 8 - ((int)white_step->to[1] - 48) % 9;
+        board[step_digit][step_liter] = white_step->figure;
+    } else {
+        printf(ANSI_COLOR_RED
+               "In " ANSI_COLOR_GREEN "%s" ANSI_COLOR_RED
+               " line. ERROR. Figure " ANSI_COLOR_GREEN "%c" ANSI_COLOR_RED
+               " not found on " ANSI_COLOR_GREEN "%s" ANSI_COLOR_RED
+               " field.\n",
+               white_step->num,
+               white_step->figure,
+               white_step->from);
+        printf("ERROR>>FIGURE_NOT_ON_FIELD_FROM\nExiting..."
+               "\n" ANSI_COLOR_RESET);
+        exit(1);
+    }
 }
