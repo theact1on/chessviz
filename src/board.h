e5039b782a2cdc2b7531852b4bf0af39895b3bad
Maxim Luparev <luparev22@list.ru>
2019-03-26 18:45:02 +0700

    step_white structure declaration

--- src/board.h
+++ src/board.h
@@ -6,5 +6,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct step_white {
+    char num[5] = {""};
+    char figure = 'P';
+    char from[3] = {""};
+    char to[3] = {""};
+    char how = ' ';
+    char shah_mat = ' ';
+    void clean()
+    {
+        figure = 'P';
+        from[0] = (char)0;
+        from[1] = (char)0;
+        from[2] = (char)0;
+        to[0] = (char)0;
+        to[1] = (char)0;
+        to[2] = (char)0;
+        how = (char)0;
+        shah_mat = (char)0;
+    }
+};
+
 void checkSteps(char* txt, char board[][8]);
 #endif
