692308adc824bc71d847bf3b140600960c4d26c3
Maxim Luparev <luparev22@list.ru>
2019-04-10 23:53:12 +0700

    Add default checks in check_move.c

--- src/check_move.c
+++ src/check_move.c
@@ -3,4 +3,32 @@
 
 int checkMove(struct step_white* figure, char board[][8])
 {
+    int from_liter = (int)figure->from[0] - 97;
+    int from_digit = 8 - ((int)figure->from[1] - 48) % 9;
+    int to_liter = (int)figure->to[0] - 97;
+    int to_digit = 8 - ((int)figure->to[1] - 48) % 9;
+
+    if (from_liter == to_liter && from_digit == to_digit) {
+        return 0;
+    }
+    if (figure->figure != board[from_digit][from_liter]) {
+        return 3;
+    }
+    if ((int)board[to_digit][to_liter] == ' ' && figure->how == 'x') {
+        return 4;
+    }
+    if ((int)board[to_digit][to_liter] != ' ' && figure->how == '-') {
+        return 5;
+    }
+    if ((int)board[to_digit][to_liter] >= 97
+        && (int)board[to_digit][to_liter] <= 122 && (int)figure->figure >= 97
+        && (int)figure->figure <= 122) {
+        return 6;
+    }
+    if ((int)board[to_digit][to_liter] >= 65
+        && (int)board[to_digit][to_liter] <= 90 && (int)figure->figure >= 65
+        && (int)figure->figure <= 90) {
+        return 6;
+    }
+    return 0;
 }
