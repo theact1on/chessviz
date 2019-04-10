5b030e08da7cfe52ae1ba5009a0b6f31e0560782
Maxim Luparev <luparev22@list.ru>
2019-04-10 23:49:11 +0700

    Create lib check_move and function checkMove

---
+++ src/check_move.c
@@ -0,0 +1,6 @@
+#include "check_move.h"
+#include "board_read.h"
+
+int checkMove(struct step_white* figure, char board[][8])
+{
+}
---
+++ src/check_move.h
@@ -0,0 +1,10 @@
+#ifndef CHECK_MOVE_H
+#define CHECK_MOVE_H
+
+#include "board_read.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int checkMove(struct step_white* figure, char board[][8]);
+
+#endif
